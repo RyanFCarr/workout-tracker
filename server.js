81edc637b16ce5c8763a751fdedc065d5b30e008
Ryan Carr <ryanfcarr@gmail.com>
2020-06-24 17:03:33 -0400

    radomized port

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.use(express.static('public'));
 app.use(require('./routes/api-routes'));
 app.use(require('./routes/html-routes'));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('App running on port 3000!');
 });
